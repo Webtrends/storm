dd0f8d37bc1684168ecc65268d12c68016da6758
Kendrick Martin <Kendrick.Martin@Webtrends.com>
2013-07-29 11:03:27 -0700

    Adding storm cookbook from Cookbooks repo

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'storm'
+maintainer       "Webtrends, Inc."
+maintainer_email "kendrick.martin@webtrends.com"
+license          "All rights reserved"
+description      "Installs Twitter's Storm distributed computation system"
+version          "1.2.5"
+depends          "java"
+depends          "runit"
+supports         "ubuntu"
