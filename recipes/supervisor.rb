38684956ba5f3973be9a51069e30240f7679175c
Eric Herot <eric.github@herot.com>
2013-10-18 15:21:41 -0400

    Access java home from node attribute; restart supervisor on storm.yaml change

--- recipes/supervisor.rb
+++ recipes/supervisor.rb
@@ -18,28 +18,26 @@
 
 include_recipe 'apache_storm'
 
-java_home = node['java']['java_home']
+# control file
+template "#{node['storm']['install_dir']}/bin/supervisor-control" do
+  source  "supervisor-control.erb"
+  mode  00755
+  variables({
+    :install_dir => node['storm']['install_dir'],
+    :log_dir => node['storm']['log_dir'],
+    :java_home => node['java']['java_home']
+  })
+end
 
-  # control file
-  template "#{node['storm']['install_dir']}/bin/supervisor-control" do
-    source  "supervisor-control.erb"
-    mode  00755
-    variables({
-      :install_dir => node['storm']['install_dir'],
-      :log_dir => node['storm']['log_dir'],
-      :java_home => java_home
-    })
-  end
-
-  # runit service
-  runit_service "supervisor" do
-    service_name "supervisor"
-    options({
-      :install_dir => "#{node['storm']['root_dir']}/current",
-      :log_dir => node['storm']['log_dir'],
-      :user => "storm"
-    })
-  end
+# runit service
+runit_service "supervisor" do
+  service_name "supervisor"
+  options({
+    :install_dir => "#{node['storm']['root_dir']}/current",
+    :log_dir => node['storm']['log_dir'],
+    :user => "storm"
+  })
+end
 
 execute "reload_supervisor" do
   command "sv reload supervisor"
@@ -47,3 +45,6 @@ execute "reload_supervisor" do
 end
 
 service "supervisor"
+
+t = resources(:template => "#{node['storm']['conf_dir']}/storm.yaml")
+t.notifies :restart, "service[supervisor]"
