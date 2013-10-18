c78bc7265a242d06d010c63ff11f3ff6e7add305
Eric Herot <eric.github@herot.com>
2013-10-18 15:15:57 -0400

    Version bump, add et_jzmq external dependency

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,10 @@ maintainer       'EverTrue, Inc.'
 maintainer_email 'devops@evertrue.com'
 license          "All rights reserved"
 description      "Installs Twitter's Storm distributed computation system"
-version          "1.2.5"
+version          "2.0.0"
+
 depends          "java"
 depends          "runit"
+depends          "et_jzmq", '= 0.1.0'
+
 supports         "ubuntu"
