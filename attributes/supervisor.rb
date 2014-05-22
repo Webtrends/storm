cf0333837cadd5ec8a5fbce3e0994b8b88209264
Eric Herot <eric.github@herot.com>
2014-05-22 18:27:25 -0400

    Build the storm yaml file programmatically rather than hard-coding a ton of options directly into the template
    Conflicts:
    	attributes/default.rb
    	attributes/nimbus.rb
    	attributes/supervisor.rb
    	attributes/zookeeper.rb
    	recipes/default.rb
    	templates/default/storm.yaml.erb

--- attributes/supervisor.rb
+++ attributes/supervisor.rb
@@ -1,7 +1,8 @@
 default['storm']['supervisor']['workers'] = 4
-default['storm']['supervisor']['childopts'] = '-Xmx1024m'
-default['storm']['supervisor']['worker_start_timeout'] = 120
-default['storm']['supervisor']['worker_timeout_secs'] = 30
-default['storm']['supervisor']['monitor_frequecy_secs'] = 3
-default['storm']['supervisor']['heartbeat_frequency_secs'] = 5
-default['storm']['supervisor']['enable'] = true
+
+default['storm']['yaml']['supervisor.childopts'] = '-Xmx1024m'
+default['storm']['yaml']['supervisor.worker.start.timeout.secs'] = 120
+default['storm']['yaml']['supervisor.worker.timeout.secs'] = 30
+default['storm']['yaml']['supervisor.monitor.frequency.secs'] = 3
+default['storm']['yaml']['supervisor.heartbeat.frequency.secs'] = 5
+default['storm']['yaml']['supervisor.enable'] = true
--- attributes/zookeeper.rb
+++ attributes/zookeeper.rb
@@ -1,5 +1,5 @@
-default['storm']['zookeeper']['port'] = 2181
-default['storm']['zookeeper']['root'] = '/storm'
-default['storm']['zookeeper']['session_timeout'] = 30_000
-default['storm']['zookeeper']['retry_times'] = 60
-default['storm']['zookeeper']['retry_interval'] = 5000
+default['storm']['yaml']['storm.zookeeper.port'] = 2181
+default['storm']['yaml']['storm.zookeeper.root'] = '/storm'
+default['storm']['yaml']['storm.zookeeper.session.timeout'] = 30_000
+default['storm']['yaml']['storm.zookeeper.retry.times'] = 60
+default['storm']['yaml']['storm.zookeeper.retry.interval'] = 5000
