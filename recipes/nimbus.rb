evertrue/apache_storm

Linting cleanup

recipes/nimbus.rb
@@ -18,7 +18,7 @@
 
 node.set['storm']['nimbus']['host'] = node['fqdn']
 
-include_recipe "storm"
+include_recipe 'storm'
 
 java_home = node['java']['java_home']
 
@@ -26,29 +26,29 @@ java_home = node['java']['java_home']
   # control file
   template "#{node['storm']['install_dir']}/bin/#{daemon}-control" do
     source  "#{daemon}-control.erb"
-    owner "root"
-    group "root"
+    owner 'root'
+    group 'root'
     mode  00755
-    variables({
-      :install_dir => node['storm']['install_dir'],
-      :log_dir => node['storm']['log_dir'],
-      :java_home => java_home
-    })
+    variables(
+      install_dir: node['storm']['install_dir'],
+      log_dir: node['storm']['log_dir'],
+      java_home: java_home
+    )
   end
 
   # runit service
   runit_service daemon do
-    options({
-      :install_dir => node['storm']['install_dir'],
-      :log_dir => node['storm']['log_dir'],
-      :user => "storm"
-    })
+    options(
+      install_dir: node['storm']['install_dir'],
+      log_dir: node['storm']['log_dir'],
+      user: 'storm'
+    )
   end
 end
 
-service "nimbus"
-service "stormui"
+service 'nimbus'
+service 'stormui'
 
-t = resources(:template => "#{node['storm']['conf_dir']}/storm.yaml")
-t.notifies :restart, "service[nimbus]"
-t.notifies :restart, "service[stormui]"
+t = resources(template: "#{node['storm']['conf_dir']}/storm.yaml")
+t.notifies :restart, 'service[nimbus]'
+t.notifies :restart, 'service[stormui]'

Webtrends/storm

recipes/nimbus.rb
@@ -18,7 +18,7 @@
 
 node.set['storm']['nimbus']['host'] = node['fqdn']
 
-include_recipe "apache_storm"
+include_recipe 'apache_storm'
 
 java_home = node['java']['java_home']
 
@@ -26,29 +26,29 @@ java_home = node['java']['java_home']
   # control file
   template "#{node['storm']['install_dir']}/bin/#{daemon}-control" do
     source  "#{daemon}-control.erb"
-    owner "root"
-    group "root"
+    owner 'root'
+    group 'root'
     mode  00755
-    variables({
-      :install_dir => node['storm']['install_dir'],
-      :log_dir => node['storm']['log_dir'],
-      :java_home => java_home
-    })
+    variables(
+      install_dir: node['storm']['install_dir'],
+      log_dir: node['storm']['log_dir'],
+      java_home: java_home
+    )
   end
 
   # runit service
   runit_service daemon do
-    options({
-      :install_dir => node['storm']['install_dir'],
-      :log_dir => node['storm']['log_dir'],
-      :user => "storm"
-    })
+    options(
+      install_dir: node['storm']['install_dir'],
+      log_dir: node['storm']['log_dir'],
+      user: 'storm'
+    )
   end
 end
 
-service "nimbus"
-service "stormui"
+service 'nimbus'
+service 'stormui'
 
-t = resources(:template => "#{node['storm']['conf_dir']}/storm.yaml")
-t.notifies :restart, "service[nimbus]"
-t.notifies :restart, "service[stormui]"
+t = resources(template: "#{node['storm']['conf_dir']}/storm.yaml")
+t.notifies :restart, 'service[nimbus]'
+t.notifies :restart, 'service[stormui]'